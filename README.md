03a1e88bfd5daea6b925a5715746c1470e09db60
Laurent <laurent@lanightlife.com>
2020-07-16 02:20:30 -0700

    adding train file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cv_image_captioning
+
+Simple image captioning system using a Sequence to Sequence model
+
+LSTM model combined with a CNN image embedder and word embeddings.
+
+## training
+
+At every time step, we look at the current caption word as input and combine it with the hidden state of the LSTM cell to produce an output. This output is then passed to the fully connected layer that produces a distribution that represents the most likely next word.
--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class EncoderCNN(nn.Module):
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
@@ -19,26 +19,54 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        """
+        :param vocab_size: size of vocabulary
+        """
         super(DecoderRNN, self).__init__()
-        
-        self.embedding = nn.Embedding(vocab_size, embed_size)
-        
-        self.lstm = nn.LSTM(input_size = embed_size,hidden_size = hidden_size,
-                            num_layers = num_layers, batch_first = True)
-        
+
+        # an Embedding module containing vocab_size tensors of size embed_size
+        # The input to the module is a list of indices,
+        # and the output is the corresponding word embeddings.
+        self.embedding = nn.Embedding(
+                num_embeddings = vocab_size,
+                embedding_dim = embed_size)
+
+        # input_size – The number of expected features in the input x
+        # hidden_size – The number of features in the hidden state h
+        # num_layers – Number of recurrent layers
+        self.lstm = nn.LSTM(
+                input_size = embed_size,
+                hidden_size = hidden_size,
+                num_layers = num_layers,
+                batch_first = True)
+
         self.linear = nn.Linear(hidden_size, vocab_size)
-        
+
     def forward(self, features, captions):
         """
         Forward propagation.
+        features - embedded image feature vector
+        captions - caption tensors (torch.Tensor(caption).long() see in data_loader __getitem__)
         """
+
+        # transforming a sequence of words (caption) into a sequence of numerical values
+        # a vector of "embed_size" numbers where each number maps to a specific word in our vocabulary
         embedded = self.embedding(captions)
-        lstm_outputs, _ = self.lstm(embedded)
+
+        # linking image features and captions to create an input of vector of size embed_size
+        # dim - dimension over which the tensors are concatenated
+        input = torch.cat((features.unsqueeze(1), embedded), dim = 1)
+
+        # lstm_outputs - tensor containing the output features (h_t) from the last layer of the LSTM, for each t.
+        # input - needs to be of size embed_size
+        lstm_outputs, _ = self.lstm(input)
+
         out = self.linear(lstm_outputs)
+
         return out
 
     def sample(self, inputs, states=None, max_len=20):
@@ -50,6 +78,6 @@ class DecoderRNN(nn.Module):
             out = self.linear(lstm_outputs)
             last_pick = out.max(1)[1]
             output_sentence.append(last_pick.item())
-            inputs = self.embedding_layer(last_pick).unsqueeze(1)
-        
+            inputs = self.embedding(last_pick).unsqueeze(1)
+
         return output_sentence
